bd8a667e6494d18a9a9f64deebee4937f04a23ba
EsmeReyes25 <me.centenoreyes@ugto.mx>
2023-06-07 15:19:12 -0600

    Modals to edit, delete and enroll added

--- teacher/course.php
+++ teacher/course.php
@@ -43,7 +43,47 @@
     <br>
     <h6>Students enrolled: <?php echo $course['students'] ?></h6>
     <br><br>
-    <a href="../teacher/edit.php?id=<?php echo $course['course_id'] ?>" class="btn btn-primary">Edit</a>
-    <a href="../teacher/delete.php?id=<?php echo $course['course_id'] ?>" class="btn btn-danger">Delete</a>
+    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdropEdit">Edit</button>
+    <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Delete</button>
+
+    <!-- Modal Delete -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Confirm</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" style="display: flex; flex-direction: column; align-items: center; text-align: center;">
+                    Are you sure you want to delete the course? <br> This action can not be undone
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <a href="../teacher/delete.php?id=<?php echo $course['course_id'] ?>" type="button" class="btn btn-danger">Delete</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal Edit -->
+    <div class="modal fade" id="staticBackdropEdit" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Confirm</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" style="display: flex; flex-direction: column; align-items: center; text-align: center;">
+                    Do you want to edit the course?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <a href="../teacher/edit.php?id=<?php echo $course['course_id'] ?>" class="btn btn-warning">Edit</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
